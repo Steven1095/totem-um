57cb1168c560c2e5b45f38d30a49d261da00a594
Steven1095 <yomarstevenmier@gmail.com>
2024-08-09 17:34:37 -0500

    home-totem-2.0

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'slider-nav': '#003333',
+				'botones': '#a8cf45',  
+				'botones-hover': '#65913e'
+			  },
+		},
+	},
+	plugins: [],
+}
