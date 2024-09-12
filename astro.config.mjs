6f11b8538b697e5deee3f23ea2a2bfbfcb00e2bd
Steven1095 <yomarstevenmier@gmail.com>
2024-09-12 15:47:52 -0500

    Galeria Slider 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,13 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
+  base: process.env.NODE_ENV === 'production' ? '/totem-um/dist/' : '/',
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    host: true, // Permite que el servidor sea accesible desde otros dispositivos si es necesario
+  }
   // output: "server",
   // adapter: node({
   //   /*mode: "standalone"*/ /// Solo contenido estático
